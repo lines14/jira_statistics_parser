e5e60118af63204e54ac424c55468b2f5cdecb12
lines14 <tonybass1414@yahoo.com>
2025-05-23 02:36:36 +0500

    up

--- modules/main/utils/data/dataUtils.js
+++ modules/main/utils/data/dataUtils.js
@@ -11,9 +11,33 @@ class DataUtils {
     fs.writeFileSync(`./artifacts/${name}.json`, JSON.stringify(data, replacer, 4));
   }
 
+  static normalizeTimestamp(timestamp) { 
+    return timestamp.replace(/([+-]\d{2})(\d{2})$/, '$1:$2')
+  };
+
   static sortByTimestamps(changelog) {
-    const normalize = (timestamp) => timestamp.replace(/([+-]\d{2})(\d{2})$/, '$1:$2');
-    return changelog.sort((a, b) => new Date(normalize(a.created)) - new Date(normalize(b.created)));
+    return changelog.sort((a, b) => new Date(this.normalizeTimestamp(a.created)) - new Date(this.normalizeTimestamp(b.created)));
+  }
+
+  static convertTimestampsToDateObjects(changelogItems) { 
+    return changelogItems.map((changelogItem) => ({ transitionFrom: changelogItem.transitionFrom, created: this.normalizeTimestamp(changelogItem.created) })).map((changelogItem) => ({ transitionFrom: changelogItem.transitionFrom, created: new Date(changelogItem.created) }));
+  }
+
+  static getTimeIntervals(changelogItems) {
+    const timeIntervals = [];
+    for (let i = 0; i < changelogItems.length - 1; i++) {
+      timeIntervals.push([changelogItems[i], changelogItems[i + 1]]);
+    }
+
+    return timeIntervals;
+  }
+
+  static intervalsOverlapping([startFirstInterval, endFirstInterval], [startSecondInterval, endSecondInterval]) {
+    return { transitionFrom: endSecondInterval.transitionFrom, overlap: startFirstInterval.created < endSecondInterval.created && startSecondInterval.created < endFirstInterval.created };
+  }
+
+  static checkIntervalsOverlap(firstIntervals, secondIntervals) {
+    return firstIntervals.map((firstInterval) => secondIntervals.map((secondInterval) => this.intervalsOverlapping(firstInterval, secondInterval)));
   }
 
   static filterCommentsWithStatuses(data, commentCreated) {
@@ -41,37 +65,93 @@ class DataUtils {
 
   static linkDevsWithBugs(issueWithBugs) {
     if (issueWithBugs.commentsWithBugs.length > 0) {
-      // issueWithBugs.changelog.forEach((element) => console.log(element.items.length));
-      // const linkedCommentsWithBugs = issueWithBugs.changelog.map((element) => ({ created: element.created, id: element.id,   }))
-      const linkedCommentsWithBugs = issueWithBugs.commentsWithBugs.map((commentWithBug) => {
-        const assigneeChangeTimestamps = [];
-        const workingStatusEndTimestamps = [];
+      return issueWithBugs.commentsWithBugs.map((commentWithBug) => {
+        const devStatusEnds = [];
+        const assigneeChanges = [];
         const linkedCommentWithBug = { ...commentWithBug };
         const sortedChangelog = this.sortByTimestamps(issueWithBugs.changelog);
-        const initialTimestamp = sortedChangelog[0].created;
+        const initialTimestamp = { 
+          transitionFrom: 'INIT TASK', 
+          created: sortedChangelog[0].created 
+        };
+
         for (const element of sortedChangelog) {
-          if (element.items.some((item) => item.field === 'status' 
-          && item.fromString 
-          && JSONLoader.config.issueStatuses.includes(item.fromString.toUpperCase()))) {
-            workingStatusEndTimestamps.push(element.created);
-          }
+          element.items.forEach((item) => { // filter includes only BACKLOG, TO DO and IN PROGRESS statuses
+            if (item.field === 'status' && item.fromString && JSONLoader.config.issueStatuses.includes(item.fromString.toUpperCase())) {
+              devStatusEnds.push({ transitionFrom: item.fromString, created: element.created });
+            }
+          });
         }
 
-        workingStatusEndTimestamps.unshift(initialTimestamp);
         for (const element of sortedChangelog) {
-          if (element.items.some((item) => item.field === 'assignee' 
-          && item.fromString)) {
-            assigneeChangeTimestamps.push(element.created);
-          }
+          element.items.forEach((item) => {
+            if (item.field === 'assignee' && item.fromString) {
+              assigneeChanges.push({ transitionFrom: item.fromString, created: element.created });
+            }
+          });
         }
         
-        assigneeChangeTimestamps.unshift(initialTimestamp);
-        console.log(workingStatusEndTimestamps);
-        return linkedCommentWithBug;
+        // filter not includes issues with only one assignee or status due to lack of transition
+        if (assigneeChanges.length > 0 && devStatusEnds.length > 0) {
+          devStatusEnds.unshift(initialTimestamp);
+          assigneeChanges.unshift(initialTimestamp);
+          const devStatusEndTimeIntervals = this.getTimeIntervals(this.convertTimestampsToDateObjects(devStatusEnds));
+          const assigneeChangeTimeIntervals = this.getTimeIntervals(this.convertTimestampsToDateObjects(assigneeChanges));
+          const result = this.checkIntervalsOverlap(devStatusEndTimeIntervals, assigneeChangeTimeIntervals);
+          console.log(result);
+          return linkedCommentWithBug;
+        }
       });
-      // throw new Error('kek');
     }
   }
 }
 
 export default DataUtils;
+
+
+
+
+
+
+
+
+
+
+
+  // static getTimeIntervalsOverlapTime(startFirstInterval, endFirstInterval, startSecondInterval, endSecondInterval) {
+  //   const overlapStart = new Date(Math.max(startFirstInterval.getTime(), startSecondInterval.getTime()));
+  //   const overlapEnd = new Date(Math.min(endFirstInterval.getTime(), endSecondInterval.getTime()));
+  //   return Math.max(0, overlapEnd - overlapStart);
+  // }
+
+  // static checkTimeIntervalsOverlapMoreThanHalf(firstIntervals, secondIntervals) {
+  //   return firstIntervals.map(([startFirstInterval, endFirstInterval]) => {
+  //     const firstIntervalDuration = endFirstInterval.created - startFirstInterval.created;
+  //     return secondIntervals.some(([startSecondInterval, endSecondInterval]) => {
+  //       const secondIntervalDuration = endSecondInterval.created - startSecondInterval.created;
+  //       const overlap = this.getTimeIntervalsOverlapTime(startFirstInterval.created, endFirstInterval.created, startSecondInterval.created, endSecondInterval.created);
+  //       return { transitionFrom: endSecondInterval.transitionFrom, longOverlap: overlap > 0.5 * firstIntervalDuration || overlap > 0.5 * secondIntervalDuration };
+  //     });
+  //   });
+  // }
+
+  //   static checkTimeIntervalsOverlapMoreThanHalf(firstIntervals, secondIntervals) {
+  //   return firstIntervals.map(([startFirstInterval, endFirstInterval]) => {
+  //     return secondIntervals.map(([startSecondInterval, endSecondInterval]) => {
+  //       const overlap = this.getTimeIntervalsOverlapTime(startFirstInterval.created, endFirstInterval.created, startSecondInterval.created, endSecondInterval.created);
+  //       // return { transitionFrom: endSecondInterval.transitionFrom, overlap: overlap > 0 };
+  //       return  overlap > 0;
+  //     });
+  //   });
+  // }
+
+  // static checkIntervalsOverlap(firstIntervals, secondIntervals) {
+  //   return firstIntervals.map(([startA, endA]) => {
+  //     return secondIntervals.some(([startB, endB]) => {
+  //       return this.getTimeIntervalsOverlapTime(
+  //         startA.created, endA.created,
+  //         startB.created, endB.created
+  //       ) > 0;
+  //     });
+  //   });
+  // }
